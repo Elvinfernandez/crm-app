f5a46e1d1613af484689f20ca8ed20b93c864dcd
Elvin Fernandez <fernandezelvin10@gmail.com>
2022-07-10 10:14:37 +0000

    Completed lead feature development.
    Handing over the Lead feature to QA team for testing purpose.

---
+++ core/lead.py
@@ -0,0 +1,16 @@
+##
+# Lead feature funcationalities.
+##
+
+
+# Create lead.
+def create_lead();
+ Print "Created lead successfully."
+
+# Update lead.
+def update_lead();
+ Print "Lead updated successfully."
+
+ # Delete lead.
+ def delete_lead();
+  Print "Lead deleted successfully."
